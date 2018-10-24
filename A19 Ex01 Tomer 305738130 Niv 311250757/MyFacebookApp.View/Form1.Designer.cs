79351a1c00380a22a437cef6336753992260ff6d
Niv Dunay <nivdu@mta.ac.il>
2018-10-24 19:28:25 +0300

    TomerTheGayButton Added

--- A19 Ex01 Tomer 305738130 Niv 311250757/MyFacebookApp.View/Form1.Designer.cs
+++ A19 Ex01 Tomer 305738130 Niv 311250757/MyFacebookApp.View/Form1.Designer.cs
@@ -37,8 +37,9 @@
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(182, 38);
 			this.button1.TabIndex = 0;
-			this.button1.Text = "NivTheGay";
+			this.button1.Text = "TomerTheGay";
 			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// Form1
 			// 
